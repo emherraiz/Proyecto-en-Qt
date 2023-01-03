fad789cbdd97f762e1f829aa790466d76f1d49e2
Enrique Marina <eherraiz@myuax.com>
2023-01-03 19:45:20 +0100

    fin

---
+++ calculadora.py
@@ -0,0 +1,91 @@
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Qt
+from PySide6.QtUiTools import QUiLoader
+from pathlib import Path
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loader = QUiLoader()
+        self.ui = loader.load('calculadora.ui')
+        self.ui.show()
+        self.ui.pushButton_0.clicked.connect(self.digito_0)
+        self.ui.pushButton_1.clicked.connect(self.digito_1)
+        self.ui.pushButton_2.clicked.connect(self.digito_2)
+        self.ui.pushButton_3.clicked.connect(self.digito_3)
+        self.ui.pushButton_4.clicked.connect(self.digito_4)
+        self.ui.pushButton_5.clicked.connect(self.digito_5)
+        self.ui.pushButton_6.clicked.connect(self.digito_6)
+        self.ui.pushButton_7.clicked.connect(self.digito_7)
+        self.ui.pushButton_8.clicked.connect(self.digito_8)
+        self.ui.pushButton_9.clicked.connect(self.digito_9)
+        self.ui.pushButton_punto.clicked.connect(self.digito_punto)
+        self.ui.pushButton_suma.clicked.connect(self.suma)
+        self.ui.pushButton_resta.clicked.connect(self.resta)
+        self.ui.pushButton_multiplicacion.clicked.connect(self.multiplicacion)
+        self.ui.pushButton_division.clicked.connect(self.division)
+        self.ui.pushButton_igual.clicked.connect(self.igual)
+
+
+    def digito_1(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '1')
+
+    def digito_2(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '2')
+
+    def digito_3(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '3')
+
+    def digito_4(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '4')
+
+    def digito_5(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '5')
+
+    def digito_6(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '6')
+
+    def digito_7(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '7')
+
+    def digito_8(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '8')
+
+    def digito_9(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '9')
+
+    def digito_0(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '0')
+
+    def digito_punto(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '.')
+
+    def suma(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '+')
+
+    def resta(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '-')
+
+    def multiplicacion(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '*')
+
+    def division(self):
+        self.ui.Linea_de_resultados.setText(self.ui.Linea_de_resultados.text() + '/')
+
+    def igual(self):
+        try:
+            self.ui.Linea_de_resultados.setText(str(eval(self.ui.Linea_de_resultados.text())))
+        except:
+            self.ui.Linea_de_resultados.setText('Error')
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
--- prueba.py
+++ prueba.py
@@ -1,21 +1,85 @@
-import wx
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QFormLayout, QWidget, QLabel,
+    QRadioButton, QCheckBox, QLineEdit, QSpinBox, QDoubleSpinBox,
+    QPushButton, QComboBox, QFontComboBox, QDateEdit, QDateTimeEdit,
+    QLCDNumber, QProgressBar, QDial, QSlider, QGridLayout, QStyle)
+from PySide6.QtCore import Qt
+from pathlib import Path
 import sys
-class MainWindow(wx.Frame):
-    # Creamos nuestra ventana a partir de un Frame
+import qtawesome as qta
+
+iconos = ['SP_ArrowBack', 'SP_ArrowDown', 'SP_ArrowForward', 
+'SP_ArrowLeft', 'SP_ArrowRight', 'SP_ArrowUp', 'SP_BrowserReload', 
+'SP_BrowserStop', 'SP_CommandLink', 'SP_ComputerIcon', 
+'SP_CustomBase', 'SP_DesktopIcon', 'SP_DialogApplyButton', 
+'SP_DialogCancelButton', 'SP_DialogCloseButton', 
+'SP_DialogDiscardButton', 'SP_DialogHelpButton', 'SP_DialogNoButton', 
+'SP_DialogOkButton', 'SP_DialogOpenButton', 'SP_DialogResetButton', 
+'SP_DialogSaveButton', 'SP_DialogYesButton', 'SP_DirClosedIcon', 
+'SP_DirHomeIcon', 'SP_DirIcon', 'SP_DirLinkIcon', 'SP_DirOpenIcon', 
+'SP_DockWidgetCloseButton', 'SP_DriveCDIcon', 'SP_DriveDVDIcon', 
+'SP_DriveFDIcon', 'SP_DriveHDIcon', 'SP_DriveNetIcon', 
+'SP_FileDialogBack', 'SP_FileDialogContentsView', 
+'SP_FileDialogDetailedView', 'SP_FileDialogEnd', 
+'SP_FileDialogInfoView', 'SP_FileDialogListView', 
+'SP_FileDialogNewFolder', 'SP_FileDialogStart', 
+'SP_FileDialogToParent', 'SP_FileIcon', 'SP_FileLinkIcon', 
+'SP_MediaPause', 'SP_MediaPlay', 'SP_MediaSeekBackward', 
+'SP_MediaSeekForward', 'SP_MediaSkipBackward', 'SP_MediaSkipForward', 
+'SP_MediaStop', 'SP_MediaVolume', 'SP_MediaVolumeMuted', 
+'SP_MessageBoxCritical', 'SP_MessageBoxInformation', 
+'SP_MessageBoxQuestion', 'SP_MessageBoxWarning', 
+'SP_TitleBarCloseButton', 'SP_TitleBarContextHelpButton', 
+'SP_TitleBarMaxButton', 'SP_TitleBarMenuButton', 
+'SP_TitleBarMinButton', 'SP_TitleBarNormalButton', 
+'SP_TitleBarShadeButton', 'SP_TitleBarUnshadeButton', 
+'SP_ToolBarHorizontalExtensionButton', 
+'SP_ToolBarVerticalExtensionButton', 'SP_TrashIcon', 'SP_VistaShield']
+
+
+def absPath(file):
+    return str(Path(__file__).parent.absolute() / file)
+class MainWindow(QMainWindow):
     def __init__(self):
-        # Con super ejecutamos su propio constructor y le pasamos los argumentos
-        # Así se crea la ventana en su propia instancia self
-        super().__init__(None, wx.ID_ANY, size=(200, 100))
-        button = wx.Button(self, wx.ID_ANY, "Hola")
-        button.Bind(wx.EVT_BUTTON, self.hola)
-        # Finalmente mostramos la ventana
-        self.Show(True)
-    # Se requiere un parámetro de argumentos indeterminados en los métodos
-    def hola(self, *args):
-        print("¡Hola mundo!")
-# Creamos la aplicación, la ventana e iniciamos el bucle
-print("\n================================")
-print(sys.argv)
-app = wx.App()
-window = MainWindow()
-app.MainLoop()
+        super().__init__()
+        # creo un layaout en cuadrícula
+        layout = QGridLayout()
+
+        # recorro los iconos con un contador de posicion
+        for contador, nombre in enumerate(iconos):
+            # recupero el icono a partir de su nombre
+            icono = self.style().standardIcon(getattr(QStyle, nombre))
+            # creo un botón con el icono y su nombre del icono
+            boton = QPushButton(icono, nombre)
+            # añado el boton en una cuadrícula de 5 columnas
+            # divido el contador entre 5 para conseguir la fila
+            # con el módulo de la divisón entre 5 conseguiré la columna
+            layout.addWidget(boton, contador // 5, contador % 5)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+
+        widget = QWidget()
+
+        self.cargarQSS("qss/ElegantDark.qss")
+        self.cargarQSS("qss/ChatBee.qss")
+        self.cargarQSS("qss/EasyCode.qss")
+
+
+    def cargarQSS(self, file):
+        # guardamos la ruta absoluta al fichero
+        path = absPath(file)
+        # intentamos abrirlo y volcar el contenido
+        try:
+            with open(path) as styles:
+                self.setStyleSheet(styles.read())
+        # si hay algún fallo lo capturamos con una excepción genérica
+        except:
+            print("Error abriendo estilos", path)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
